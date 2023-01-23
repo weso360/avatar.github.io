c4b70cdd8897b19c8781ad98458ec61d8b8628ec
WS-code <115361234+weso360@users.noreply.github.com>
2023-01-23 01:54:29 +0000

    Add files via upload

---
+++ avatar.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      //the initial line of the ject.
+      Console.WriteLine(" .-."); 
+      //second part of the avatar   
+      Console.WriteLine("(o ø)");
+      //third part of the avatar
+      Console.WriteLine("| O |");
+      //the avatars body
+       Console.WriteLine("|   |");
+        Console.WriteLine("'~~~'");
+        //avatars name
+        Console.WriteLine("Stylo AKA Mr.Squdrific");
+     }
+  }
+}
